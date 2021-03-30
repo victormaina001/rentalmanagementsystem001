1a74d3dc66830fcb3f0e47a3dcb79cabd707c22e
VICTOR MAINA <ngaruiyamaina001@gmail.com>
2021-03-30 18:09:18 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,11 +6,11 @@ const mysql = require('mysql');
  // Connection
 var pool = mysql.createPool({
     connectionLimit : 100,
-    host            : process.env.DB_HOST,
-    port            : process.env.DB_PORT,
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASS,
-    database        : process.env.DB_NAME,
+    host            : process.env.RDS_HOSTNAME,
+    port            : process.env.RDS_PORT,
+    user            : process.env.RDS_USERNAME,
+    password        : process.env.RDS_PASSWORD,
+    database        : process.env.RDS_DB_NAME,
 
 });
 
