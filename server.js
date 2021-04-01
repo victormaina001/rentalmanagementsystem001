954065ded5100304bb9a1f7336b9cd7e364e5103
VICTOR MAINA <ngaruiyamaina001@gmail.com>
2021-04-01 15:30:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.set('view engine', 'hbs');
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
